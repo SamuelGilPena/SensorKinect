4a4b2ab4171550505baaa96b9ffd3433f90fc179
Jonas Jongejan <jonas@halfdanj.dk>
2011-01-06 02:14:25 +0100

    Added Mac support and updated to newest unstable branch from PrimeSense/Sensor code to work with the unstable branch of OpenNI

--- Source/XnDeviceSensorV2/XnExportedSensorDevice.cpp
+++ Source/XnDeviceSensorV2/XnExportedSensorDevice.cpp
@@ -1,24 +1,24 @@
 /*****************************************************************************
-*                                                                            *
-*  PrimeSense Sensor 5.0 Alpha                                               *
-*  Copyright (C) 2010 PrimeSense Ltd.                                        *
-*                                                                            *
-*  This file is part of PrimeSense Common.                                   *
-*                                                                            *
-*  PrimeSense Sensor is free software: you can redistribute it and/or modify *
-*  it under the terms of the GNU Lesser General Public License as published  *
-*  by the Free Software Foundation, either version 3 of the License, or      *
-*  (at your option) any later version.                                       *
-*                                                                            *
-*  PrimeSense Sensor is distributed in the hope that it will be useful,      *
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              *
-*  GNU Lesser General Public License for more details.                       *
-*                                                                            *
-*  You should have received a copy of the GNU Lesser General Public License  *
-*  along with PrimeSense Sensor. If not, see <http://www.gnu.org/licenses/>. *
-*                                                                            *
-*****************************************************************************/
+ *                                                                            *
+ *  PrimeSense Sensor 5.0 Alpha                                               *
+ *  Copyright (C) 2010 PrimeSense Ltd.                                        *
+ *                                                                            *
+ *  This file is part of PrimeSense Common.                                   *
+ *                                                                            *
+ *  PrimeSense Sensor is free software: you can redistribute it and/or modify *
+ *  it under the terms of the GNU Lesser General Public License as published  *
+ *  by the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  PrimeSense Sensor is distributed in the hope that it will be useful,      *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              *
+ *  GNU Lesser General Public License for more details.                       *
+ *                                                                            *
+ *  You should have received a copy of the GNU Lesser General Public License  *
+ *  along with PrimeSense Sensor. If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                            *
+ *****************************************************************************/
 
 
 
@@ -45,7 +45,7 @@ void XnExportedSensorDevice::FillCommonDescriptionFields(XnProductionNodeDescrip
 {
 	strcpy(pDescription->strName, XN_DEVICE_NAME);
 	strcpy(pDescription->strVendor, XN_VENDOR_PRIMESENSE);
-
+	
 	pDescription->Version.nMajor = XN_PS_MAJOR_VERSION;
 	pDescription->Version.nMinor = XN_PS_MINOR_VERSION;
 	pDescription->Version.nMaintenance = XN_PS_MAINTENANCE_VERSION;
@@ -61,10 +61,10 @@ void XnExportedSensorDevice::GetDescription(XnProductionNodeDescription* pDescri
 XnStatus XnExportedSensorDevice::EnumerateProductionTrees(xn::Context& context, xn::NodeInfoList& TreesList, xn::EnumerationErrors* pErrors)
 {
 	XnStatus nRetVal = XN_STATUS_OK;
-
+	
 	// enumerate connected sensors
 	XnUInt32 nCount = 0;
-
+	
 	// check if sensor is connected
 	nRetVal = XnSensor::Enumerate(NULL, &nCount);
 	if (nRetVal != XN_STATUS_OUTPUT_BUFFER_OVERFLOW)
@@ -72,21 +72,21 @@ XnStatus XnExportedSensorDevice::EnumerateProductionTrees(xn::Context& context, 
 		// no sensor connected
 		XN_LOG_WARNING_RETURN(XN_STATUS_DEVICE_NOT_CONNECTED, XN_MASK_DEVICE_SENSOR, "No PS sensor is connected!");
 	}
-
+	
 	// allocate according to count
 	XnConnectionString* pConnStrings;
 	XN_VALIDATE_CALLOC(pConnStrings, XnConnectionString, nCount);
-
+	
 	nRetVal = XnSensor::Enumerate(pConnStrings, &nCount);
 	if (nRetVal != XN_STATUS_OK)
 	{
 		xnOSFree(pConnStrings);
 		return (nRetVal);
 	}
-
+	
 	XnProductionNodeDescription Description;
 	GetDescription(&Description);
-
+	
 	// each connection string is a sensor. return it
 	for (XnUInt32 i = 0; i < nCount; ++i)
 	{
@@ -97,29 +97,34 @@ XnStatus XnExportedSensorDevice::EnumerateProductionTrees(xn::Context& context, 
 			return (nRetVal);
 		}
 	}
-
+	
 	xnOSFree(pConnStrings);
-
+	
 	return (XN_STATUS_OK);
 }
 
 XnStatus XnExportedSensorDevice::Create(xn::Context& context, const XnChar* strInstanceName, const XnChar* strCreationInfo, xn::NodeInfoList* pNeededTrees, const XnChar* strConfigurationDir, xn::ModuleProductionNode** ppInstance)
 {
 	XnStatus nRetVal = XN_STATUS_OK;
-
+	
 	XnChar strGlobalConfigFile[XN_FILE_MAX_PATH];
 	nRetVal = XnSensor::ResolveGlobalConfigFileName(strGlobalConfigFile, XN_FILE_MAX_PATH, strConfigurationDir);
 	XN_IS_STATUS_OK(nRetVal);
-
+	
+#if (XN_PLATFORM == XN_PLATFORM_MACOSX)
+	XnBool bEnableMultiProcess = FALSE;
+#else
+	
 	XnBool bEnableMultiProcess = TRUE;
 	XnUInt32 nValue;
 	if (XN_STATUS_OK == xnOSReadIntFromINI(strGlobalConfigFile, XN_CONFIG_FILE_SERVER_SECTION, XN_MODULE_PROPERTY_ENABLE_MULTI_PROCESS, &nValue))
 	{
 		bEnableMultiProcess = (nValue == TRUE);
 	}
-
+#endif
+	
 	XnDeviceBase* pSensor = NULL;
-
+	
 	if (bEnableMultiProcess)
 	{
 		XN_VALIDATE_NEW(pSensor, XnSensorClient);
@@ -128,13 +133,13 @@ XnStatus XnExportedSensorDevice::Create(xn::Context& context, const XnChar* strI
 	{
 		XN_VALIDATE_NEW(pSensor, XnSensor);
 	}
-
+	
 	XnDeviceConfig config;
 	config.DeviceMode = XN_DEVICE_MODE_READ;
 	config.cpConnectionString = strCreationInfo;
 	config.SharingMode = XN_DEVICE_EXCLUSIVE;
 	config.pInitialValues = NULL;
-
+	
 	if (strConfigurationDir != NULL)
 	{
 		if (bEnableMultiProcess)
@@ -148,30 +153,30 @@ XnStatus XnExportedSensorDevice::Create(xn::Context& context, const XnChar* strI
 			pActualSensor->SetGlobalConfigFile(strGlobalConfigFile);
 		}
 	}
-
+	
 	nRetVal = pSensor->Init(&config);
 	if (nRetVal != XN_STATUS_OK)
 	{
 		XN_DELETE(pSensor);
 		return (nRetVal);
 	}
-
+	
 	XnSensorDevice* pDevice = XN_NEW(XnSensorDevice, context, pSensor, strInstanceName);
 	if (pDevice == NULL)
 	{
 		XN_DELETE(pSensor);
 		return (XN_STATUS_ALLOC_FAILED);
 	}
-
+	
 	nRetVal = pDevice->Init();
 	if (nRetVal != XN_STATUS_OK)
 	{
 		XN_DELETE(pSensor);
 		return (nRetVal);
 	}
-
+	
 	*ppInstance = pDevice;
-
+	
 	return (XN_STATUS_OK);
 }
 
